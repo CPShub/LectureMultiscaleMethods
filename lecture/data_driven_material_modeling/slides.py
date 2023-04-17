33b07c548289e0f3f21085af1c1bdcdf9bce25ae
Dominik Klein <62878150+klein-tu-da@users.noreply.github.com>
2023-04-17 12:47:02 +0200

    Deleted more TF stuff

--- lecture/data_driven_material_modeling/slides.py
+++
@@ -1,8 +0,0 @@
-import glob
-import subprocess
-
-#for notebook in glob.glob("*.ipynb"):
-for notebook in ['part1.ipynb', 'part2.ipynb']:
-    print("make slides for: ", notebook)
-    command = "jupyter nbconvert " + notebook + " --to slides --SlidesExporter.reveal_scroll=True"
-    subprocess.run(command, shell=True)
